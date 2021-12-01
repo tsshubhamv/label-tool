4b3e88e67ad7a3b6f47c22eb4d46eb6c2e4de82e
Shubham Vishwakarma <shubham.vishwakarma@toothsi.in>
2021-12-01 16:52:44 +0530

    Minor Chnages

--- server/migrations/001-init.up.sql
+++ server/migrations/001-init.up.sql
@@ -16,6 +16,7 @@ create table Images (
   labelData text not null, /* JSON-stringified data, matching the form data in the project */
   lastEdited real default 0.0,
   projectsId integer,
+  callbackUrl text,
   constraint images_fk_projectsId foreign key (projectsId) references Projects (id) on delete cascade
 );
 
@@ -26,5 +27,5 @@ create table MLModels (
   type text not null
 );
 
-insert into projects (name, form) values ('Test Project', '{ "formParts": [ { "type": "polygon", "name": "Car", "id": "nfjxui" }, { "type": "bbox", "name": "Windows", "id": "n3ndi88" } ] }');
-insert into images (originalName, link, labeled, labelData, projectsId) values ('tesla.jpg', '/uploads/1/1.jpg', 0, '{ }', 1);
+-- insert into projects (name, form) values ('Test Project', '{ "formParts": [ { "type": "polygon", "name": "Car", "id": "nfjxui" }, { "type": "bbox", "name": "Windows", "id": "n3ndi88" } ] }');
+-- insert into images (originalName, link, labeled, labelData, projectsId) values ('tesla.jpg', '/uploads/1/1.jpg', 0, '{ }', 1);
